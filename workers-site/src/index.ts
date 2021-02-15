d8ede7a4be2c133f6ffc9692e5bf1d9c7b401ab8
azu <azuciao@gmail.com>
2021-02-16 00:44:34 +0900

    feat(workers): kv impl

--- workers-site/src/index.ts
+++ workers-site/src/index.ts
@@ -1,19 +1,13 @@
 import { Router, Sunder } from "sunder";
 import cookie from "cookie";
-import "@cloudflare/workers-types";
 
 type User = {
-    id: string;
     name: string;
-    spreadsheetId: string;
+    spreadsheetId?: string;
     google_token: string;
 };
-type UserPage = {
-    id: string;
-    spreadsheetJSON: object;
-};
 
-declare const USER_NAMESPACE: KVNamespace;
+declare const USERS: KVNamespace;
 
 const app = new Sunder();
 const router = new Router();
@@ -22,6 +16,37 @@ const createRandom = () => {
     crypto.getRandomValues(arr);
     return Array.from(arr, (dec) => dec.toString(16).padStart(2, "0")).join("");
 };
+// _next proxy
+router.get("/_next/(.*)", async (context) => {
+    const cache = caches.default;
+    const pathname = new URL(context.request.url).pathname;
+    const cacheKey = context.event.request;
+    const response = await cache.match(cacheKey);
+    if (response && response.body) {
+        context.response.body = response.body;
+        return;
+    }
+    const proxyURL = `https://philan-net.vercel.app${pathname}`;
+    const fetchedResponse = await fetch(proxyURL);
+    await cache.put(cacheKey, fetchedResponse.clone());
+    if (fetchedResponse.body) context.response.body = fetchedResponse.body;
+    else context.response.body = "No response";
+});
+router.get("/user/:user", async ({ request, response, params }) => {
+    const user = params.user;
+    const text = await USERS.get(user);
+    if (!text) {
+        response.body = "No user";
+        return;
+    }
+    const userData = JSON.parse(text) as User;
+    const SSRURL = `https://philan-net.vercel.app/user/1FNux_fbqZsbTJpJ3AIcC3-RF7oywV7uJ8TDwZv19T2k/${userData.google_token}`;
+    const res = await fetch(SSRURL);
+    if (res.body) response.body = res.body;
+    else {
+        response.body = "No response";
+    }
+});
 router.get("/auth", ({ response, params }) => {
     const uuid = createRandom();
     response.set("Set-Cookie", `philan-state=${uuid}; Secure; HttpOnly`);
@@ -39,8 +64,12 @@ router.get("/auth/callback", async (ctx) => {
     const url = "https://philan-net.vercel.app/api/auth/getToken?" + code;
     const res = await fetch(url);
     const refreshToken = await res.json();
-    // Store? Refresh Token with user
-    console.log("fetched", JSON.stringify(refreshToken, null, 4));
+    const user: User = {
+        name: "azu",
+        google_token: refreshToken.tokens.access_token
+    };
+    await USERS.put("azu", JSON.stringify(user));
+    ctx.response.body = JSON.stringify(refreshToken, null, 4);
 });
 app.use(async (ctx, next) => {
     const start = Date.now();

f302b8ce352aa3e5b2b4c6fb3fffbdd8e926c857
azu <azuciao@gmail.com>
2021-02-23 22:38:10 +0900

    fix: fix auth

--- api/api-utils/create-OAuth.ts
+++ api/api-utils/create-OAuth.ts
@@ -3,7 +3,10 @@ import { google, Auth } from "googleapis";
 export const createOAuthClient = (credentials?: Auth.Credentials) => {
     const CLIENT_ID = process.env.CLIENT_ID;
     const CLIENT_SECRET = process.env.CLIENT_SECRET;
-    const REDIRECT_URL = "http://localhost:3000/api/auth/authorized";
+    const REDIRECT_URL =
+        process.env.NODE_ENV === "development"
+            ? "http://localhost:3000/api/auth/authorized"
+            : "https://philan-net.vercel.app/api/auth/authorized";
     const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
     if (credentials) {
         oAuth2Client.setCredentials(credentials);

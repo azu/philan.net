45e437a153b6404200f6eee7cfb85f6b0c8fd985
azu <azuciao@gmail.com>
2021-03-08 00:01:54 +0900

    chore: comment

--- web/pages/user/[userId]/index.tsx
+++ web/pages/user/[userId]/index.tsx
@@ -244,7 +244,7 @@ export async function getStaticProps({
         },
         // Next.js will attempt to re-generate the page:
         // - When a request comes in
-        // - At most once every second
+        // - At most once x second
         revalidate: 60 // In seconds
     };
 }

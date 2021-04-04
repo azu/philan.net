81eae03ac3c84510734cf8fb4a789ecaf51fec74
azu <azuciao@gmail.com>
2021-04-04 17:46:45 +0900

    feat(web): add favicon

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -11,6 +11,9 @@ class CustomDocument extends Document<Props> {
             <Html lang={"ja-JP"}>
                 <Head>
                     <meta name="description" content="Philan.netは、自身の寄付内容を公開、管理できるサービスです。" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png" />
                     {/* No cookie Analytics https://www.cloudflare.com/web-analytics/ */}
                     {process.env.NODE_ENV === "production" ? (
                         <script

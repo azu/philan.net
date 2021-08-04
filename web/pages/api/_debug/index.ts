4686ef913d2185b372d5ca195f9985263b34e6e4
azu <azuciao@gmail.com>
2021-08-04 19:48:04 +0900

    feat: add debug

--- web/pages/api/_debug/index.ts
+++ web/pages/api/_debug/index.ts
@@ -1,6 +1,6 @@
 import nextConnect from "next-connect";
 
-const handler = nextConnect().get(async () => {
+const handler = nextConnect().get(async (req, res) => {
     console.log(JSON.stringify(process.env));
     res.json({});
 });
